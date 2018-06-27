867b464a88207244622105ad291baed811079c5f
Shiv <shivapaul98@gmail.com>
2018-06-27 11:35:49 +0530

    Created location screen with basic info

--- app/src/main/java/com/shiv/gastimate/LocationActivity.java
+++ app/src/main/java/com/shiv/gastimate/LocationActivity.java
@@ -4,6 +4,7 @@ package com.shiv.gastimate;
  * Created by Shiv Paul on 6/25/2018.
  */
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,13 +12,14 @@ import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity
 {
+    @SuppressLint("DefaultLocale")
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.location_activity);
 
-        TextView textView = findViewById(R.id.textView);
+        TextView textView = findViewById(R.id.carSummary);
         Intent intent = getIntent();
         double mpg = intent.getDoubleExtra("vehicleMpg", 0.0);
         textView.setText(String.format("Vehicle Name: %s \nVehicle MPG: %f", intent.getStringExtra("vehicleName"), mpg));
---
+++ app/src/main/java/com/shiv/gastimate/SetLocationActivity.java
@@ -0,0 +1,18 @@
+package com.shiv.gastimate;
+
+/*
+ * Created by Shiv Paul on 6/25/2018.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetLocationActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_location_activity);
+    }
+}
