eeb8aff0f1feae4c3fdf6e23d4f2e525b919f332
Shiv <shivapaul98@gmail.com>
2018-06-27 13:54:32 +0530

    Made set location screen

--- app/src/main/java/com/shiv/gastimate/LocationActivity.java
+++ app/src/main/java/com/shiv/gastimate/LocationActivity.java
@@ -8,6 +8,8 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 import android.widget.TextView;
 
 public class LocationActivity extends AppCompatActivity
@@ -23,5 +25,29 @@ public class LocationActivity extends AppCompatActivity
         Intent intent = getIntent();
         double mpg = intent.getDoubleExtra("vehicleMpg", 0.0);
         textView.setText(String.format("Vehicle Name: %s \nVehicle MPG: %f", intent.getStringExtra("vehicleName"), mpg));
+
+        CardView fromCardView = findViewById(R.id.fromCardView);
+        fromCardView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(LocationActivity.this, SetLocationActivity.class);
+                intent.putExtra("locationType", 0); //0 if from, 1 if to
+                startActivity(intent);
+            }
+        });
+
+        CardView toCardView = findViewById(R.id.toCardView);
+        toCardView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(LocationActivity.this, SetLocationActivity.class);
+                intent.putExtra("locationType", 1); //0 if from, 1 if to
+                startActivity(intent);
+            }
+        });
     }
 }
