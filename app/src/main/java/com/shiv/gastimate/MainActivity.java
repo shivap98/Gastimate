33071b0e3799bf638d5c67b25ac63b76f5068ded
Shiv Paul <shivapaul98@gmail.com>
2018-08-11 13:41:05 +0530

    Set up MainActivity edit mode UI
    Added FABs and Edit Mode notifier

--- app/src/main/java/com/shiv/gastimate/MainActivity.java
+++ app/src/main/java/com/shiv/gastimate/MainActivity.java
@@ -5,8 +5,10 @@ package com.shiv.gastimate;
  */
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +16,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -30,8 +33,12 @@ public class MainActivity extends AppCompatActivity
     public static ArrayList<Vehicle> vehicles;
     public static Vehicle currentVehicle;
 
-    public static boolean dbChanged;
+    public static boolean editMode = false;
+    public static boolean dbChanged;    //true if list needs to be refreshed
+    boolean fabOpen = false;      //true if mainButton is expanded
 
+    FloatingActionButton mainButton;
+    TextView editModeDisplay;
     /**
      * Called when activity is created
      *
@@ -50,32 +57,9 @@ public class MainActivity extends AppCompatActivity
         vehiclesList.setLayoutManager(new LinearLayoutManager(this));
         vehiclesList.setAdapter(new VehicleListAdapter(vehicles));
 
-        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
-        floatingActionButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MainActivity.this, AddVehicleActivity.class);
-                startActivity(intent);
-            }
-        });
-        floatingActionButton.setOnLongClickListener(new View.OnLongClickListener()
-        {
-            @Override
-            public boolean onLongClick(View view)
-            {
-                vehicles.add(new Vehicle("Car 1", "Make 1", "Model 1", 2014, 1.1, 11.11, NOT_TRACKING, 0, CAR));
-                vehicles.add(new Vehicle("Motorbike 1", "Make 2", "Model 2", 2015, 2.1, 21.11, NOT_TRACKING, 0, MOTORCYCLE));
-                vehicles.add(new Vehicle("Other 1", "Make 3", "Model 3", 2016, 3.1, 31.11, 0, NOT_TRACKING, OTHER));
+        setFabListeners();
 
-                vehiclesList.setAdapter(null);
-                vehiclesList.setAdapter(new VehicleListAdapter(vehicles));
-
-                Log.i("FAB LongPress", "Added sample vehicles");
-                return true;
-            }
-        });
+        editModeDisplay = findViewById(R.id.editModeDisplay);
     }
 
     /**
@@ -115,6 +99,71 @@ public class MainActivity extends AppCompatActivity
     }
 
     /**
+     * Sets the actions and listeners for the FABs
+     */
+    void setFabListeners()
+    {
+        final FloatingActionButton addButton = findViewById(R.id.addButton);
+        addButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MainActivity.this, AddVehicleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        final FloatingActionButton editButton = findViewById(R.id.editButton);
+        editButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                mainButton.callOnClick();       //collapses the mainButton
+                editMode = true;
+                mainButton.setImageResource(R.drawable.ic_close);
+                editModeDisplay.setVisibility(View.VISIBLE);
+            }
+        });
+
+        addButton.hide();
+        editButton.hide();
+
+        mainButton = findViewById(R.id.mainButton);
+        mainButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                if(editMode)
+                {
+                    editMode = false;
+                    mainButton.setImageResource(R.drawable.ic_unfold);
+                    editModeDisplay.setVisibility(View.GONE);
+                }
+                else
+                {
+                    if(fabOpen)
+                    {
+                        fabOpen = false;
+                        addButton.hide();
+                        editButton.hide();
+                        mainButton.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorAccent)));
+                    }
+                    else
+                    {
+                        fabOpen = true;
+                        addButton.show();
+                        editButton.show();
+                        mainButton.setBackgroundTintList(ColorStateList.valueOf(getColor(R.color.colorAccentDark)));
+                    }
+                }
+            }
+        });
+    }
+
+    /**
      * Called when coming back here from location activity, scrolls back up
      */
     @Override
@@ -124,6 +173,10 @@ public class MainActivity extends AppCompatActivity
         if(dbChanged)
         {
             readVehicles();
+        }
+        if(fabOpen)
+        {
+            mainButton.callOnClick();
         }
         vehiclesList.scrollToPosition(0);
     }
